7d3153b5dfdb8e369b2fe083cc4131e674b66d8f
siavash <sia@siarez.com>
2017-06-22 13:04:35 -0700

    gitignore fix

--- _site/README.md
+++
@@ -1,3 +0,0 @@
-## Welcome to siarez.com 
-
-This is the personal website of [Sia Rezaei](https://www.linkedin.com/in/siavash-rezaei-69b8542a/). Cool stuff soon to be added.
--- _site/assets/js/main.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    $('img.zoomable').on('click', function() {
-        $('.enlargeImageModalSource').attr('src', $(this).attr('src'));
-        $('#enlargeImageModal').modal('show');
-    });
-});
